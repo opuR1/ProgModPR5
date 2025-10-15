a2760bd8f7e7ba6edc9267bac8b0e4293a8c2472
opuR1 <lel070320@gmail.com>
2025-10-15 18:57:44 +0700

    Начало работы(БД подключена)

---
+++ ProgModPR5/Helper.cs
@@ -0,0 +1,23 @@
+﻿using ProgModPR5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgModPR5
+{
+    internal class Helper
+    {
+        private static ProgramModPR5Entities1 _context;
+
+        public static ProgramModPR5Entities1 GetContext()
+        {
+            if (_context == null)
+            {
+                _context = new ProgramModPR5Entities1();
+            }
+            return _context;
+        }
+    }
+}
